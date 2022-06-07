b05b6c6fd4915eb812c10709b1188517eb9fd939
allexib <ooomegasib@gmail.com>
2022-06-07 20:20:38 +0300

    theme fixes syncfusion

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -18,6 +18,7 @@ import { useStateContext } from "../contexts/ContextProvider";
 import product9 from "../data/product9.jpg";
 
 const Ecommerce = () => {
+  const { currentColor } = useStateContext();
   return (
     <div className="mt-12">
       <div className="flex flex-wrap lg:flex-nowrap justify-center ">
@@ -31,7 +32,7 @@ const Ecommerce = () => {
           <div className="mt-6">
             <Button
               color="white"
-              bgColor="blue"
+              bgColor={currentColor}
               text="Download"
               borderRadius="10px"
               size="md"
@@ -107,21 +108,21 @@ const Ecommerce = () => {
                   height="80px"
                   width="250px"
                   data={SparklineAreaData}
-                  color="blue"
+                  color={currentColor}
                 />
               </div>
-                <div className="mt-10">
-                    <Button
-                        color="white"
-                        bgColor='blue'
-                        text="Download Report"
-                        borderRadius="10px"
-                    />
-                </div>
-            </div>
-              <div>
-                  <Stacked currentMode='red' width="320px" height="360px" />
+              <div className="mt-10">
+                <Button
+                  color="white"
+                  bgColor={currentColor}
+                  text="Download Report"
+                  borderRadius="10px"
+                />
               </div>
+            </div>
+            <div>
+              <Stacked currentMode="red" width="320px" height="360px" />
+            </div>
           </div>
         </div>
       </div>
